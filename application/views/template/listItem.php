a281f99123353c7b929305180d31990106febadd
Unknown <fubautsu@gmail.com>
2017-02-28 20:49:39 +0100

    Modification de l'affichage des items dans la liste

--- application/views/template/listItem.php
+++ application/views/template/listItem.php
@@ -12,30 +12,32 @@
                 <img src="<?php echo base_url('asset/userfile/img/' . $item['category_id'] . '/' . $item['subcategory_id'] . '/' . $item['item_img']) ?>" title="<?php echo $item['item_name']; ?>" />
             </div>
             <div class="front" data-href="<?php echo 'home/item/' . $item['item_id']; ?>">
-                <div class="info">
-                    <p><?php echo $item['item_name']; ?></p>
-                    <p><?php echo $item['item_descript']; ?></p>
-                </div>
-                <div class="icons">
-                    <div class="float-left">
+                <div>
+                    <h1 class="text-center"><?php echo $item['item_name']; ?></h1>
+                    <p class="text-center">
                         <a href="<?php echo site_url('home/category/' . $item['subcategory_id']); ?>">
                             <span class="icon-<?php echo $item['category_icon']; ?>" title="<?php echo $item['main_category'] . ' - ' . $item['sub_category']; ?>"></span>
                         </a>
-                    </div>
-                    <div class="float-right">
-                        <a href="<?php echo site_url('user/manageItem/edit/' . $item['item_id']); ?>" 
-                           class="edit <?php echo in_array($this->session->user['id'], $possessors) ? '' : 'hidden'; ?>"
-                           title="Modifier l'item">
-                            <span class="icon-pencil"></span>
-                        </a>
+                        
                         <?php if(!in_array($this->session->user['id'], $possessors)) : ?>
                         <span class="icon-box-add borrow <?php echo !in_array($this->session->user['id'], $borrowers) ? '' : 'disabled'; ?>" 
-                              data-id="<?php echo $item['item_id']; ?>"
-                              title="Demander l'emprunt de l'item"></span>
+                               data-id="<?php echo $item['item_id']; ?>"
+                               title="Demander l'emprunt de l'item"></span>
                         <?php endif; ?>
+                    </p>
+                </div>
+                <div class="icons">
+                    <div class="float-left">
+                        <a href="<?php echo site_url('user/manageItem/edit/' . $item['item_id']); ?>" 
+                            class="edit <?php echo in_array($this->session->user['id'], $possessors) ? '' : 'hidden'; ?>"
+                            title="Modifier l'item">
+                            <span class="icon-pencil"></span>
+                        </a>
+                    </div>
+                    <div class="float-right">
                         <span class="<?php echo in_array($this->session->user['id'], $possessors) ? 'icon-checkbox-checked' : 'icon-checkbox-unchecked' ?> possess"
-                              data-id="<?php echo $item['item_id']; ?>"
-                              title="<?php echo in_array($this->session->user['id'], $possessors) ? 'Supprimer de ma collection' : 'Ajouter à ma collection' ?>"></span>
+                               data-id="<?php echo $item['item_id']; ?>"
+                               title="<?php echo in_array($this->session->user['id'], $possessors) ? 'Supprimer de ma collection' : 'Ajouter à ma collection' ?>"></span>
                     </div>
                     <div class="clearfix"></div>
                 </div>
