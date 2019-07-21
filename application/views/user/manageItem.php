ffa9ea4fb9db845602864a0d983f7343732bd7d3
Unknown <fubautsu@gmail.com>
2019-07-21 17:45:08 +0200

    Remise en place du formulaire
    Remise en place du formulaire d'ajout d'item

--- application/views/user/manageItem.php
+++ application/views/user/manageItem.php
@@ -1,5 +1,5 @@
 <h1 class="text-center">Création d'un item</h1>
-<div id="stepWrapper">
+<form name="manageItem" enctype="multipart/form-data" method="POST" action=""  id="stepWrapper">
     <div id="step1" class="step">
         <h2>Veuillez choisir une catégorie</h2>
         <div>
@@ -17,4 +17,36 @@
             
         </div>
     </div>
-</div>
+    <div id="step3" class="step">
+        <h2>Veuillez remplir les infos item</h2>
+        <div>
+            <div id="imgContainer" class="col-xs-12">
+                <img src="" />
+            </div>
+            <div id="imgInput" class="col-xs-12">
+                <div class="row">
+                    <div class="col-xs-4">
+                        <div class="box">
+                            <label for="imgItem">Image (<?php echo $maxWidthImg . 'x' . $maxHeightImg; ?>, 2Mo max.)</label>
+                        </div>
+                    </div>
+                    <div class="col-xs-8">
+                        <div class="box">
+                            <input type="file" name="item_img" id="imgItem" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12">
+                <div class="floatingLabel">
+                    <input type="text" name="item_name" id="nameItem" maxlength="255" <?php echo isset($item) ? 'value="' . $item['item_name'] . '"' : ''; ?> />
+                    <label for="nameItem">Nom</label>
+                </div>
+            </div>
+        </div>
+        <button type="submit">Terminer</button>
+    </div>
+    
+    <input type="hidden" id="categoryId" name="category_id" />
+    <input type="hidden" id="subCategoryId" name="subcategory_id" />
+</form>
--- asset-dev/js/user/manageItem.js
+++ asset-dev/js/user/manageItem.js
@@ -3,6 +3,7 @@
     // Gestion de létape 1 (Choix de la catégorie)
     $("#step1 .category").on("click", function(){
         var id = $(this).data("id");
+        $("#categoryId").val(id);
         $.ajax({
             url: siteUrl + "/user/manageItem",
             type: "POST",
@@ -36,9 +37,26 @@
         if($target.hasClass("category") || $target.parents(".category").length > 0) {
             if($target.parents(".category").length > 0) {
                 $target = $target.parents(".category");
+                $("#subCategoryId").val($target.data("id"));
+                $.ajax({
+                    url: siteUrl + "/user/manageItem",
+                    type: "POST",
+                    data: {
+                        cmd: "getCompl",
+                        category: $target.find("p").text().toLowerCase()
+                    },
+                    dataType: "JSON",
+                    success: function(data) {
+                        changeStep("next");
+                        $("#step3 > div").append(data.html);
+                        setFloatingLabel();
+                    }
+                });
             }
         }
     });
+    
+    
     
     /**
      * Gestion des diverses étapes du formulaire
@@ -63,6 +81,44 @@
         }
     }
     changeStep("first");
+    
+    // Gestion de la prévisualisation de l'image
+    var reader = new FileReader();
+    
+    reader.onload = function (e) {
+        $('#imgContainer img').attr('src', e.target.result);
+    }
+
+    $("#imgItem").on("change", function(){
+        var files = this.files;
+        
+        if(files && files[0]) {
+            reader.readAsDataURL(files[0]);
+        }
+    });
+    
+    // Gestion de l'ajout/suppression des pistes d'un album
+    var trackTemplate = $(
+        '<div class="floatingLabel track">' + 
+            '<input type="text" name="track[]" value="" />' +
+        '</div>'
+    );
+    
+    document.querySelector("body").addEventListener("click", function(e){
+        var $target = $(e.target);
+        if($target.is("#addTrack")) {
+            $target.parents("#manageTrack").before(trackTemplate.clone());
+        }
+        else if ($target.is("#delTrack")) {
+            var allTrack = $target.parents(".compl").find("[name*='track']");
+            if(allTrack.length === 1) {
+                showAlertBox("Impossible de supprimer la dernière piste", "error");
+            }
+            else {
+                allTrack.last().parent().remove();
+            }
+        }
+    });
     
     
     // Chargement des sous-catégories
