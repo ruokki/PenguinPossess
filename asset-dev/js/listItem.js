257dc89d18cde8c1e788ff1b17926d77bbc9c5a2
Unknown <fubautsu@gmail.com>
2020-01-09 16:03:06 +0100

    Correction bug après filtre
    Correction bug après filtrage de la liste (désactivait la gestion des clics)

--- asset-dev/js/listItem.js
+++ asset-dev/js/listItem.js
@@ -15,103 +15,109 @@
             },
             success: function(data) {
                 $(".listItem, #listItem").html(data);
+                setItemEvents();
             }
         });
     });
     
-    $(".item .front, #item .category").on("click", function(e){
-        // On veut ajouter/enlever la possession d'un objet
-        if($(e.target).hasClass("possess")) {
-            var $possess = $(e.target),
-                cmd = $possess.hasClass("icon-checkbox-checked") ? "delPossess" : "addPossess",
-                id = $possess.data("id");
-                
-            $.ajax({
-                type: "POST",
-                url: siteUrl + "/home/managePossess",
-                data: {
-                    cmd: cmd,
-                    item: id
-                },
-                success: function(data){
-                    var text = "";
-                    if(cmd === "delPossess") {
-                        text = "supprimé de";
-                        $possess.removeClass("icon-checkbox-checked")
-                            .addClass("icon-checkbox-unchecked")
-                            .attr("title", "Ajouter à ma collection");
-                        $possess.prev(".edit").addClass("hidden");
-                        
-                        if(typeof(myCollection) !== "undefined" && myCollection === true) {
-                            var $item = $possess.parents(".item");
-                            $item.on("transitionend", function(){
-                                $(this).remove();
-                            });
-                            $item.addClass("deleted");
-                        }
-                    }
-                    else if (cmd === "addPossess") {
-                        text = "ajouté à";
-                        $possess.removeClass("icon-checkbox-unchecked")
-                            .addClass("icon-checkbox-checked")
-                            .attr("title", "Supprimer de ma collection");
-                        $possess.prev(".edit").removeClass("hidden");
-                    }
-                    showAlertBox("Item " + text + " votre collection", "success");
-                }
-            });
-        }
-        // On souhaite emprunter l'item
-        else if($(e.target).hasClass("borrow")) {
-            var $possess = $(e.target),
-                cmd = "addBorrow",
-                id = $possess.data("id");
-                
-            $.ajax({
-                type: "POST",
-                url: siteUrl + "/home/managePossess",
-                data: {
-                    cmd: cmd,
-                    item: id
-                },
-                dataType: "JSON",
-                success: function(data){
-                    var possessors = data.possessors;
+    function setItemEvents() {
+        $(".item .front, #item .category").off(".toDel").on("click.toDel", function(e){
+            // On veut ajouter/enlever la possession d'un objet
+            if($(e.target).hasClass("possess")) {
+                var $possess = $(e.target),
+                    cmd = $possess.hasClass("icon-checkbox-checked") ? "delPossess" : "addPossess",
+                    id = $possess.data("id");
 
-                    if(data.error && data.error === true) {
-                        showAlertBox(data.text, "error");
+                $.ajax({
+                    type: "POST",
+                    url: siteUrl + "/home/managePossess",
+                    data: {
+                        cmd: cmd,
+                        item: id
+                    },
+                    success: function(data){
+                        var text = "";
+                        if(cmd === "delPossess") {
+                            text = "supprimé de";
+                            $possess.removeClass("icon-checkbox-checked")
+                                .addClass("icon-checkbox-unchecked")
+                                .attr("title", "Ajouter à ma collection");
+                            $possess.prev(".edit").addClass("hidden");
+
+                            if(typeof(myCollection) !== "undefined" && myCollection === true) {
+                                var $item = $possess.parents(".item");
+                                $item.on("transitionend", function(){
+                                    $(this).remove();
+                                });
+                                $item.addClass("deleted");
+                            }
+                        }
+                        else if (cmd === "addPossess") {
+                            text = "ajouté à";
+                            $possess.removeClass("icon-checkbox-unchecked")
+                                .addClass("icon-checkbox-checked")
+                                .attr("title", "Supprimer de ma collection");
+                            $possess.prev(".edit").removeClass("hidden");
+                        }
+                        showAlertBox("Item " + text + " votre collection", "success");
                     }
-                    else {
-                        if(data.created === true) {
-                            $possess.addClass("disabled");
-                            showAlertBox("Demande d'emprunt effectuée auprès de " + possessors[0].split("|")[1], "success");
+                });
+            }
+            // On souhaite emprunter l'item
+            else if($(e.target).hasClass("borrow")) {
+                var $possess = $(e.target),
+                    cmd = "addBorrow",
+                    id = $possess.data("id");
+
+                $.ajax({
+                    type: "POST",
+                    url: siteUrl + "/home/managePossess",
+                    data: {
+                        cmd: cmd,
+                        item: id
+                    },
+                    dataType: "JSON",
+                    success: function(data){
+                        var possessors = data.possessors;
+
+                        if(data.error && data.error === true) {
+                            showAlertBox(data.text, "error");
                         }
                         else {
-                            var html = "";
-                            var tmp = [];
-                            for(var i in possessors) {
-                                tmp = possessors[i].split("|");
-                                html += 
-                                    '<div class="user" data-id="' + tmp[0] + '" data-item="' + id + '">' +
-                                        '<img src="' + baseUrl + 'asset/default-user.png' + '" />' +
-                                        '<p class="text-center">' + tmp[1] + '</p>' +
-                                    '</div>';
+                            if(data.created === true) {
+                                $possess.addClass("disabled");
+                                showAlertBox("Demande d'emprunt effectuée auprès de " + possessors[0].split("|")[1], "success");
                             }
+                            else {
+                                var html = "";
+                                var tmp = [];
+                                for(var i in possessors) {
+                                    tmp = possessors[i].split("|");
+                                    html += 
+                                        '<div class="user" data-id="' + tmp[0] + '" data-item="' + id + '">' +
+                                            '<img src="' + baseUrl + 'asset/default-user.png' + '" />' +
+                                            '<p class="text-center">' + tmp[1] + '</p>' +
+                                        '</div>';
+                                }
 
-                            $modalBorrow.find(".listUser .user").remove();
-                            $modalBorrow.find(".listUser").prepend(html);
-                            $modalBorrow.find(".listUser .user").first().find(":first-child").click();
-                            $modalBorrow.dialog("open");
+                                $modalBorrow.find(".listUser .user").remove();
+                                $modalBorrow.find(".listUser").prepend(html);
+                                $modalBorrow.find(".listUser .user").first().find(":first-child").click();
+                                $modalBorrow.dialog("open");
+                            }
                         }
                     }
-                }
-            });
-        }
-        // Si on clique ailleurs que sur un lien, on redirige vers les infos item
-        else if(!$(e.target).is("a")) {
-            window.location = siteUrl + "/" + $(this).data("href");
-        }
-    });
+                });
+            }
+            // Si on clique ailleurs que sur un lien, on redirige vers les infos item
+            else if(!$(e.target).is("a")) {
+                window.location = siteUrl + "/" + $(this).data("href");
+            }
+        });
+    }
+    
+    setItemEvents();
+    
     
     var $modalBorrow = $("#modalBorrow");
     $modalBorrow.dialog({
