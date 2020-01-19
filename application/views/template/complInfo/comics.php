9b80b937dd25eac672375212e5771e10fbc06635
Unknown <fubautsu@gmail.com>
2020-01-19 18:23:47 +0100

    Correction incohérence formulaire

--- application/views/template/complInfo/comics.php
+++ application/views/template/complInfo/comics.php
@@ -1,7 +1,6 @@
 <?php if($typeView === 'print') : ?>
 <p>
     <?php $this->view('template/complInfo/input/item_creator', array('label' => 'Auteur')); ?> -
-    <?php $this->view('template/complInfo/input/item_universe'); ?> -
     <?php $this->view('template/complInfo/input/item_idx_sibling'); ?> -
     <?php $this->view('template/complInfo/input/item_editor'); ?> -
     <?php $this->view('template/complInfo/input/item_release'); ?>
@@ -9,7 +8,6 @@
 <?php else : ?>
     <p class="col-xs-12 compl attention">! Attention ! Le comics doit être un one-shot. <a href="<?php echo site_url('user/createCollection'); ?>">Pour créer une collection, cliquer ici</a></p>
     <?php $this->view('template/complInfo/input/item_creator', array('label' => 'Auteur')); ?>
-    <?php $this->view('template/complInfo/input/item_universe'); ?>
     <?php $this->view('template/complInfo/input/item_editor'); ?>
     <?php $this->view('template/complInfo/input/item_release'); ?>
 <?php endif; ?>
--- application/views/template/complInfo/livre.php
+++ application/views/template/complInfo/livre.php
@@ -1,15 +1,13 @@
 <?php if($typeView === 'print') : ?>
 <p>
     <?php $this->view('template/complInfo/input/item_creator', array('label' => 'Auteur')); ?> -
-    <?php $this->view('template/complInfo/input/item_universe'); ?> -
     <?php $this->view('template/complInfo/input/item_idx_sibling'); ?> -
     <?php $this->view('template/complInfo/input/item_editor'); ?> -
     <?php $this->view('template/complInfo/input/item_release'); ?>
 </p>
 <?php else : ?>
+    <p class="col-xs-12 compl attention">! Attention ! Le livre doit être un one-shot. <a href="<?php echo site_url('user/createCollection'); ?>">Pour créer une collection, cliquer ici</a></p>
     <?php $this->view('template/complInfo/input/item_creator', array('label' => 'Auteur')); ?>
-    <?php $this->view('template/complInfo/input/item_universe'); ?>
-    <?php $this->view('template/complInfo/input/item_idx_sibling'); ?>
     <?php $this->view('template/complInfo/input/item_editor'); ?>
     <?php $this->view('template/complInfo/input/item_release'); ?>
 <?php endif; ?>
