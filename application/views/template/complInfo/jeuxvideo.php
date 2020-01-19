1f3d4deb3cf99cbb7556683dfd87cbe8275287ac
Unknown <fubautsu@gmail.com>
2020-01-19 18:25:38 +0100

    Correction incohérence formulaire

--- application/views/template/complInfo/jeuxvideo.php
+++ application/views/template/complInfo/jeuxvideo.php
@@ -3,13 +3,13 @@
     <?php $this->view('template/complInfo/input/item_creator', array('label' => 'Studio')); ?> -
     <?php $this->view('template/complInfo/input/item_universe'); ?> -
     <?php $this->view('template/complInfo/input/item_editor'); ?> -
-    <?php $this->view('template/complInfo/input/item_type'); ?> -
-    <?php $this->view('template/complInfo/input/item_release'); ?>
+    <?php $this->view('template/complInfo/input/item_release'); ?> -
+    <?php $this->view('template/complInfo/input/item_type'); ?>
 </p>
 <?php else : ?>
     <?php $this->view('template/complInfo/input/item_creator', array('label' => 'Studio')); ?>
     <?php $this->view('template/complInfo/input/item_universe'); ?>
     <?php $this->view('template/complInfo/input/item_editor'); ?>
-    <?php $this->view('template/complInfo/input/item_type'); ?>
     <?php $this->view('template/complInfo/input/item_release'); ?>
+    <?php $this->view('template/complInfo/input/item_type'); ?>
 <?php endif; ?>
